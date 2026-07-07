498e03c81af687b3fd90e91b53df69edd442332d
jhohannessf <jhohannes.sf@gmail.com>
2026-07-06 23:29:27 -0300

    criação de método mapper

--- src/main/java/br/com/jhohannesfreitas/candidaturas/dto/UsuarioRequest.java
+++ src/main/java/br/com/jhohannesfreitas/candidaturas/dto/UsuarioRequest.java
@@ -1,5 +1,6 @@
 package br.com.jhohannesfreitas.candidaturas.dto;
 
+import br.com.jhohannesfreitas.candidaturas.model.UsuarioEntity;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
@@ -12,5 +13,22 @@ public record UsuarioRequest (
         String email,
         @NotBlank(message = "Senha obrigatória")
         String senha
-) {}
+) {
+
+    // Mapper de entrada: Transforma uma DTO em uma Entidade
+    public UsuarioEntity toEntity() {
+        UsuarioEntity usuario = new UsuarioEntity();
+        preencher(usuario);
+
+        return usuario;
+    }
+
+    public void preencher(UsuarioEntity usuario) {
+        usuario.setId(id);
+        usuario.setNome(nome);
+        usuario.setEmail(email);
+        usuario.setSenha(senha);
+    }
+
+}
 
